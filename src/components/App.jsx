9ca3729965e9304b70bf31b0b10fae6a0cd6e91a
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-07-13 18:24:04 +0200

    update
    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-// import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -12,65 +9,27 @@ import { Modal } from './Modal/Modal';
 import css from './App.module.css';
 
 export class App extends Component {
-  static defaultProps = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      page: 1,
+      images: [],
+      isLoading: false,
+      error: null,
+      selectedImage: null,
+    };
+  }
 
-  static propTypes = {};
-
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    error: null,
-    selectedImage: null,
-  };
-
-  async componentDidMount() {
-    this.setState({
-      isLoading: true,
-    });
-    const response = await axios.get(
-      `/pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=21858532-01f8fabf05f69063186fd3644&image_type=photo&orientation=horizontal&per_page=12`
-    );
-    response
-      .then(data => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...data.hits],
-          isLoading: false,
-        }));
-      })
-      .catch(error => {
-        this.setState({
-          error,
-          isLoading: false,
-        });
-      });
-    // this.setState({ data: response.data });
+  componentDidMount() {
+    this.fetchImages();
     document.addEventListener('keydown', this.handleKeyDown);
   }
 
-  async componentDidUpdate(prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
     if (prevState.query !== query || prevState.page !== page) {
-      this.setState({
-        isLoading: true,
-      });
-      const response = await axios.get(
-        `/pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=21858532-01f8fabf05f69063186fd3644&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      response
-        .then(data => {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...data.hits],
-            isLoading: false,
-          }));
-        })
-        .catch(error => {
-          this.setState({
-            error,
-            isLoading: false,
-          });
-        });
+      this.fetchImages();
     }
   }
 
@@ -78,12 +37,33 @@ export class App extends Component {
     document.removeEventListener('keydown', this.handleKeyDown);
   }
 
+  fetchImages = () => {
+    const { query, page } = this.state;
+    this.setState({ isLoading: true });
+
+    fetch(
+      `https://pixabay.com/api/?q=${query}&page=${page}&key=21858532-01f8fabf05f69063186fd3644&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+          isLoading: false,
+        }));
+      })
+      .catch(error => {
+        this.setState({ error, isLoading: false });
+      });
+  };
+
+  handleKeyDown = event => {
+    if (event.key === 'Escape') {
+      this.setState({ selectedImage: null });
+    }
+  };
+
   handleSubmit = query => {
-    this.setState({
-      query,
-      page: 1,
-      images: [],
-    });
+    this.setState({ query, page: 1, images: [] });
   };
 
   handleLoadMore = () => {
@@ -93,15 +73,16 @@ export class App extends Component {
   };
 
   handleImageSelection = imageUrl => {
-    this.selectedImage({ selectedImage: imageUrl });
+    this.setState({ selectedImage: imageUrl });
   };
 
   handleCloseModal = () => {
-    this.selectedImage({ selectedImage: null });
+    this.setState({ selectedImage: null });
   };
 
   render() {
     const { images, isLoading, error, selectedImage } = this.state;
+
     return (
       <div className={css.app}>
         <Searchbar onSubmit={this.handleSubmit} />
@@ -128,79 +109,4 @@ export class App extends Component {
   }
 }
 
-// console.log(key);
-// export const App = () => {
-//   const [query, setQuery] = useState('');
-//   const [page, setPage] = useState(1);
-//   const [images, setImages] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [selectedImage, setSelectedImage] = useState(null);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetch(
-//       `https://pixabay.com/api/?q=${query}&page=${page}&key=21858532-01f8fabf05f69063186fd3644&image_type=photo&orientation=horizontal&per_page=12`
-//     )
-//       .then(response => response.json())
-//       .then(data => {
-//         setImages(prevImages => [...prevImages, ...data.hits]);
-//         setIsLoading(false);
-//       })
-//       .catch(error => {
-//         setError(error);
-//         setIsLoading(false);
-//       });
-//   }, [query, page]);
-
-//   useEffect(() => {
-//     const handleKeyDown = event => {
-//       if (event.key === 'Escape') {
-//         setSelectedImage(null);
-//       }
-//     };
-//     document.addEventListener('keydown', handleKeyDown);
-//     return () => {
-//       document.removeEventListener('keydown', handleKeyDown);
-//     };
-//   }, []);
-
-//   const handleSubmit = query => {
-//     setQuery(query);
-//     setPage(1);
-//     setImages([]);
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage(prevPage => prevPage + 1);
-//   };
-
-//   const handleImageSelection = imageUrl => {
-//     setSelectedImage(imageUrl);
-//   };
-
-//   const handleCloseModal = () => {
-//     setSelectedImage(null);
-//   };
-
-//   return (
-//     <div className={css.app}>
-//       <Searchbar onSubmit={handleSubmit} />
-//       {error && <p>Something went wrong: {error.message}</p>}
-//       {isLoading && <Loader />}
-//       {images.length > 0 && (
-//         <>
-//           <ImageGallery
-//             images={images}
-//             id={nanoid}
-//             setSelectedImage={handleImageSelection}
-//           />
-//           <Button onClick={handleLoadMore} />
-//         </>
-//       )}
-//       {selectedImage && (
-//         <Modal imageUrl={selectedImage} onCloseModal={handleCloseModal} />
-//       )}
-//     </div>
-//   );
-// };
+export default App;
